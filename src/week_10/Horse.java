4f2e98f8c80abff284818075dc38478ecf33583f
LeeJiHeon <dlwlflgjs@naver.com>
2023-10-18 14:27:51 +0900

    10주차 10, 11번 문제

---
+++ src/week_10/Horse.java
@@ -0,0 +1,57 @@
+package week_10;
+
+public class Horse {
+    private int numOfLeg;
+    private int power;
+    private String color;
+    private int speed;
+
+    // 기본 생성자
+    public Horse() {
+        // 아무 초기화도 필요하지 않을 경우
+    }
+
+    // 파라미터를 통해 속성을 초기화하는 생성자
+    public Horse(int numOfLeg, int power, String color, int speed) {
+        this.numOfLeg = numOfLeg;
+        this.power = power;
+        this.color = color;
+        this.speed = speed;
+    }
+
+    // numOfLeg의 Setter와 Getter 메서드
+    public void setNumOfLeg(int numOfLeg) {
+        this.numOfLeg = numOfLeg;
+    }
+
+    public int getNumOfLeg() {
+        return numOfLeg;
+    }
+
+    // power의 Setter와 Getter 메서드
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    // color의 Setter와 Getter 메서드
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    // speed의 Setter와 Getter 메서드
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
