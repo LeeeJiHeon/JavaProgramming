35ca14666e416ea4299028343b092b74819373af
LeeJiHeon <dlwlflgjs@naver.com>
2023-11-06 18:43:00 +0900

    Class 최종 수정

--- src/week_9/Person.java
+++ src/week_9/Person.java
@@ -7,12 +7,40 @@ public class Person {
     String phoneNumber;		// 전화번호
     char gender;			// 성별
 	
-    @Override
+    public Person() {
+    	this(0, "", "", "", ' ');
+    }
+    
+    public Person(int number, String name, String address, String phoneNumber, char gender) {
+		super();
+		this.number = number;
+		this.name = name;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.gender = gender;
+	}
+    
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	@Override
 	public String toString() {
 		return "number = " + number + "\nname = " + name + "\naddress = " + address + "\nphoneNumber = " + phoneNumber + "\ngender = " + gender + "\n";
 	}
 
-    
-    
 }
 
