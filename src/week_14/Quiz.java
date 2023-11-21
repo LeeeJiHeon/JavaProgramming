b9dd7334e43bdd5bfbd13e4d9fcc7169f308f10f
LeeJiHeon <dlwlflgjs@naver.com>
2023-11-21 18:19:21 +0900

    14주차 Quiz

---
+++ src/week_14/Quiz.java
@@ -0,0 +1,20 @@
+package week_14;
+
+public class Quiz {
+	private String question;	// ÄûÁî
+    private String answer;		// ´äº¯
+
+    public Quiz(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+}
